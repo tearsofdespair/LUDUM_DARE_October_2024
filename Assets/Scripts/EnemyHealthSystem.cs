3bf24e14b7c0b00bd40b56e1c95bb91aa4f7d44b
tearsofdespair <mealiv2004@gmail.com>
2024-10-05 19:52:15 +0300

    Imported Skybox Package

---
+++ Assets/Scripts/EnemyHealthSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class EnemyHealthSystem : MonoBehaviour
+{
+    [SerializeField] private EnemySettings EnemySettings;
+    private float ActualHealth;
+
+    private void Awake()
+    {
+        ActualHealth = EnemySettings.HealthPoints;
+    }
+
+    public void Update()
+    {
+        if(ActualHealth <= 0)
+            Destroy(gameObject);
+    }
+
+    public void FixedUpdate()
+    {
+        ActualHealth -= 2;
+        Debug.Log(ActualHealth);
+    }
+
+}
