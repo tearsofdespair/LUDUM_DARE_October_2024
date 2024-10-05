674a84e4945dbe163a2a3b19cdc312d46ff4f437
tearsofdespair <mealiv2004@gmail.com>
2024-10-05 11:55:42 +0300

    Added Zenject and fixed movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,39 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
     public float Speed = 1;
-    private bool gotInput = false;
+    private float _horizontalAxis;
+    private float _verticalAxis;
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.AddForce(new Vector3(1, 0, 0) * Speed * Time.deltaTime, ForceMode.VelocityChange);
-            gotInput = true;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(new Vector3(-1, 0, 0) * Speed * Time.deltaTime, ForceMode.VelocityChange);
-            gotInput = true;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(new Vector3(0, 0, 1) * Speed * Time.deltaTime, ForceMode.VelocityChange);
-            gotInput = true;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(new Vector3(0, 0, -1) * Speed * Time.deltaTime, ForceMode.VelocityChange);
-            gotInput = true;
-        }
-
-        if (!gotInput)
-        {
-            rb.velocity = Vector3.zero;
-        }
+        HandleAxis();
+        MovePlayer();
     }
+
+    private void HandleAxis()
+    {
+        _horizontalAxis = Input.GetAxis("Horizontal");
+        _verticalAxis = Input.GetAxis("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 tempVec = new Vector3(_horizontalAxis, 0, _verticalAxis);
+        tempVec = tempVec.normalized * Speed;
+        rb.MovePosition(rb.position + tempVec * Time.fixedDeltaTime);
+    }
+
 }
